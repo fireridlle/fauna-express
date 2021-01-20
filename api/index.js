31fa5cd6eb2c63b15e21c6d2eebfb7ac9761196c
Sergii Zinkevych <s.zinkevych@gmail.com>
2021-01-20 15:19:36 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 'use strict';
 const express = require('express');
+const fetch = require('node-fetch');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
@@ -12,6 +13,7 @@ router.get('/', (req, res) => {
   res.end();
 });
 router.get('/env', (req, res) => res.json(process.env));
+router.get('/do', (req, res) => fetch('http://169.254.169.254/metadata/v1/').then(response => res.send(response)).catch(error => res.status(400).send(error)))
 
 app.use(bodyParser.json());
 app.use('/api', router)
