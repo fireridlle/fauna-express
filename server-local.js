1019ccec6b6e9d0aee2fb8983e359185c902a862
SZinkevych <szinkevych@lohika.com>
2021-01-21 12:09:09 +0200

    process.env.port

--- server-local.js
+++ server-local.js
@@ -2,4 +2,4 @@
 
 const app = require('./api');
 
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Local app listening on port 3000!'));
