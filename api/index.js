4f80853d42f71d5684d88087f85dd54b5aed90e5
SZinkevych <szinkevych@lohika.com>
2021-01-28 16:10:49 +0200

    bump

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
   res.end();
 });
 router.get('/fauna', async (req, res) => getUser().then(user => res.send(user)).catch(error => res.status(500).send(error)))
-router.get('/env', (req, res) => {
+router.get('/env', (_, res) => {
 
   res.json({
     ...process.env,
@@ -23,7 +23,7 @@ router.get('/env', (req, res) => {
     platform: os.platform()
   })
 });
-router.get('/do', (req, res) => fetch('http://169.254.169.254/metadata/v1/').then(response => res.send(response.data)).catch(error => res.status(400).send(error)))
+router.get('/do', (_, res) => fetch('http://169.254.169.254/metadata/v1/').then(response => res.send(response.data)).catch(error => res.status(400).send(error)))
 
 app.use(bodyParser.json());
 app.use('/api', router)
